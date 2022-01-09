21e769b9bd484ddcf7e5d15d871a1f548ae59a0c
jdk <57665888+hello-jdk@users.noreply.github.com>
2022-01-10 08:49:21 +0900

    자판기 만들기
    최종

--- Sub.java
+++ Sub.java
@@ -1,41 +1,41 @@
-/*
- * 01/05(수) 3차회의
-1. 배열크기정하기 = 10개
-2. toString으로 제품목록보여주기		
-3. 돈투입 유효성검사			
-4. 제품구매부분 				
-5. 관리자모드 				
-6. receipt 데이터 관리 코드?			
-					
-1. 제품구매부분
-2. toString
-3. 관리자모드 or 유효성,receipt 
-각자 만들어본다음에 다음날 8시에 코드리뷰
- */
+package project2;
 
 import java.util.Scanner;
 
 abstract class Beverage {
 	private static int pkNum = 1000;
-	
-	private int pk ; // DB관리를 위한 변수
+
+	private int pk; // DB관리를 위한 변수
 	private String name; // 음료수 이름
 	private int stock; // 음료수 재고
 	private int price; // 음료수 가격
-	
+
+	// 생성자
 	public Beverage(String name) {
-		this(name,0,0);
+		this(name, 0, 0);
 	}
 
-	public Beverage(String name,int stock, int price) {
+	public Beverage(String name, int stock, int price) {
 		this.name = name;
 		this.stock = stock;
 		this.price = price;
 		this.pk = pkNum++;
 	}
-	
-	
-	// Name
+
+	// 메서드
+	@Override
+	public String toString() {
+		if (stock <= 0)
+			return name + "\t(품절)";
+		else
+			return name + "\t가격:" + price;
+	}
+
+	public String toStringM() {
+		return "이름: " + name + "\t가격:" + price + "\t재고:" + stock;
+	}
+
+	// getter setter
 	public String getName() {
 		return name;
 	}
@@ -44,16 +44,17 @@ abstract class Beverage {
 		this.name = name;
 	}
 
-	// Stock
 	public int getStock() {
 		return stock;
 	}
 
 	public void setStock(int stock) {
 		this.stock = stock;
+		if (this.stock <= 0) {
+			this.stock = 0;
+		}
 	}
 
-	// Price
 	public int getPrice() {
 		return price;
 	}
@@ -69,7 +70,7 @@ class Juice extends Beverage {
 	public Juice(String name) {
 		super(name);
 	}
-	
+
 	public Juice(String name, int stock, int price) {
 		super(name, stock, price);
 	}
@@ -81,7 +82,7 @@ class CarbonatedDrink extends Beverage {
 	public CarbonatedDrink(String name) {
 		super(name);
 	}
-	
+
 	public CarbonatedDrink(String name, int stock, int price) {
 		super(name, stock, price);
 	}
@@ -89,7 +90,7 @@ class CarbonatedDrink extends Beverage {
 }
 
 class SportDrink extends Beverage {
-	
+
 	public SportDrink(String name) {
 		super(name);
 	}
@@ -103,70 +104,133 @@ class SportDrink extends Beverage {
 public class Sub {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		// 음료수[] data = 음료수 배열[크기]
-		Beverage[] data = new Beverage[10];
-		// 기본제공음료
-		data[0] = new Juice("주스");
-		data[1] = new CarbonatedDrink("탄산음료");
-		data[2] = new SportDrink("이온음료");
+
+		// 기본 생성
+		int MAX = 10;
+		Beverage[] data = new Beverage[MAX];
+		data[0] = new Juice("주스음료", 10, 1000);
+		data[1] = new CarbonatedDrink("탄산음료", 0, 500);
+		data[2] = new SportDrink("이온음료", 5, 700);
 
 		// 변수
-		// 소지금, 영수증, 시크릿코드
 		int money = 0;
-		String receipt = "";
-		int code = 0000;
+		String code = "9999";
+
+		String[] rName = new String[MAX];
+		int[] rPrice = new int[MAX];
 
 		while (true) {
 			System.out.println("===========================");
-			System.out.println("       음료수 자판기          ");
-			//toString 제품번호(배열인덱스+1)+제품이름 + 가격
-			System.out.println("1. 돈투입 2. 제품구입 3. 종료");
-			System.out.print("숫자를 입력하세요 : ");
-			System.out.println();
-			System.out.println("===========================");
-			int num = sc.nextInt();
-			
-			// 첫화면
-			// 버튼 : 돈투입, 제품구입, 종료
-
-			// 1 돈투입
-			if (num == 1) {
-				System.out.print("얼마를 입력하시겠습니까? : ");
-				//유효성검사필요
-				money += sc.nextInt(); 
-				System.out.println("현재 잔액은 " + money + "원 입니다."); // 안내문구 (현재잔액)
-				continue; // 첫화면으로
-
+			System.out.println("\t음료수 자판기");
+			int a = 1;
+			for (Beverage v : data) {
+				if (v == null)
+					continue;
+				System.out.println("[" + a++ + "]" + v);
 			}
-			// 2 제품구입
-			else if (num == 2) {
+			System.out.println("1. 돈투입 2. 제품구입 3. 종료");
+			System.out.println("===========================");
+			System.out.print("숫자를 입력하세요 >> ");
+			int num = sc.nextInt();
+
+			if (num == 1) {
+				System.out.print("얼마를 입력하시겠습니까? >> ");
+				money += sc.nextInt();
+				System.out.println("현재 잔액은 " + money + "원 입니다."); // 안내문구 (현재잔액)
+				continue;
+
+			} else if (num == 2) {
 				while (true) {
-					System.out.print("마실 음료의 숫자를 입력하세요 : ");
+					System.out.print("마실 음료의 숫자를 입력하세요 (뒤로가기 : 0) >> ");
 					int act = sc.nextInt();
-					
-					if(act == 0000) {
-						//새로만들부분
-						manage();
+
+					if (Integer.toString(act).equals(code)) {
+						System.out.println("&&&&관리자모드&&&&");
+						data = manage(data);
+
+					} else if (act >= 1 && act <= a) {
+						String cName = data[act - 1].getName();
+						int cStock = data[act - 1].getStock();
+						int cPrice = data[act - 1].getPrice();
+
+						if (cStock == 0) {
+							System.out.println(">> 품절입니다. <<");
+							continue;
+						} else if (cPrice > money) {
+							System.out.println(">> 소지금이 부족합니다. <<");
+							continue;
+						} else {
+							data[act - 1].setStock(cStock - 1); // 재고 감소
+							money -= cPrice; // 소지금 감소
+
+							// 영수증
+							boolean flag = true;
+							int empty = MAX;
+							for (int i = 0; i < rName.length; i++) {
+								if (rName[i] == null) {
+									if (empty > i) {
+										empty = i;
+									}
+									continue;
+								}
+								if (rName[i].equals(cName)) {
+									rPrice[i] += cPrice;
+									flag = false;
+									break;
+								}
+							}
+							
+							
+							if (flag) {
+								rName[empty] = cName;
+								rPrice[empty] = cPrice;
+							}
+
+							// 안내문구 깔끔하게 할려면 없애는게 나을수도
+							System.out.println("\"" + cName + "\"구입을 완료했습니다.");
+							System.out.println("현재 남은 금액: " + money + "원");
+
+							// 제품구입화면
+							continue;
+						}
 					}
-					
-					//새로만들부분
-					//제품구입
-					//배열로 받아서 정보변경
-					//1.buy로 처음부터 모든 입출력담당 2.번호입력받은후 buy로정보변경 3.buy메서드를 쓰지않고 main부분에서 처리
-			
-					else { 
+
+					// 뒤로가기
+					else if (act == 0) {
+						System.out.println("뒤로가기");
+						break;
+					}
+
+					else {
 						// 안내문구 (유효성검사)
 						System.out.println("잘못된 입력입니다. 다시 입력하세요. ");
 						continue;
 					}
-					
+
 				}
 
 			}
 			// 3 종료 및 예외성 입력
 			else if (num == 3) {
+
+				// 돈을 넣었지만 구매하지 않은 경우
+				if (rName[0] == null)
+					if (money > 0)
+						System.out.println("거스름돈 : " + money + "원");
+
 				// 영수증 출력
-				System.out.println(receipt+"를 구매하셨습니다.");
+				if (rName[0] != null) {
+					System.out.println("=============영수증=============");
+					System.out.println("제품이름\t\t\t가격");
+					for (int i = 0; i < rName.length; i++) {
+						if (rName[i] == null)
+							continue;
+						System.out.println(rName[i] + "\t\t\t" + rPrice[i] + "원");
+					}
+					if (money != 0)
+						System.out.println("\t\t거스름돈:\t" + money + "원");
+					System.out.println("==============================");
+				}
 				System.out.println("이용해주셔서 감사합니다.");
 				break;
 			} else {
@@ -179,13 +243,101 @@ public class Sub {
 
 	}
 
-	private static void manage() {
-		//관리자모드
-	}
-	
-	private static void buy(Beverage b){
-		b.getPrice();
-		b.getStock();
-	}
+	private static Beverage[] manage(Beverage[] data) {
 
+		Beverage[] nData = data;
+
+		while (true) {
+			// 관리자모드
+			Scanner sc = new Scanner(System.in);
+			System.out.println("1.목록 2.수량변경   3.물품추가 0.나가기");
+			System.out.print(">>");
+			int act = sc.nextInt();
+
+			// 물품목록보기(재고확인)
+			if (act == 1) {
+				for (Beverage v : nData) {
+					if (v == null)
+						continue;
+					System.out.println(v.toStringM());
+				}
+			}
+
+			// 수량변경
+			else if (act == 2) {
+				int a = 1;
+				for (Beverage v : nData) {
+					if (v == null)
+						continue;
+					System.out.println(
+							String.format("[%d]", a++) + v.getName() + "\t가격:" + v.getPrice() + "\t재고:" + v.getStock());
+				}
+				System.out.print("수량을 변경할 제품을 골라주세요>> ");
+				int act2 = sc.nextInt();
+				System.out.print("몇개로 변경하시겠습니까?>> ");
+				int act3 = sc.nextInt();
+				if (act2 >= 1 && act2 <= a) {
+					System.out.println("이전 수량:" + nData[act2 - 1].getStock());
+					nData[act2 - 1].setStock(act3);
+					System.out.println("변경 수량:" + nData[act2 - 1].getStock());
+				} else {
+					// 유효성 검사
+					System.out.println("목록 안에서 입력해주세요.");
+				}
+
+			}
+			// 물품추가
+			else if (act == 3) {
+
+				// 카테고리
+				int c = 0;
+				do {
+					System.out.println("카테고리를 설정해주세요.");
+					System.out.println("1.주스음료 2.탄산음료 3.이온음료");
+					System.out.print(">>");
+					c = sc.nextInt();
+				} while (c < 1 || c > 3);
+
+				// 이름입력
+				System.out.print("제품이름 >>");
+				String s = sc.next();
+				// 수량
+				System.out.print("수량 >>");
+				int n = sc.nextInt();
+				// 가격입력
+				System.out.print("가격 >>");
+				int p = sc.nextInt();
+
+				// 빈공간 찾기
+				int index = nData.length;
+				for (int i = 0; i < index; i++) {
+					if (nData[i] == null) {
+						index = i;
+						break;
+					}
+				}
+
+				if (c == 1) {
+					nData[index] = new Juice(s, n, p);
+				} else if (c == 2) {
+					nData[index] = new CarbonatedDrink(s, n, p);
+				} else {
+					nData[index] = new SportDrink(s, n, p);
+				}
+				System.out.println("추가가 완료되었습니다.");
+
+			}
+			// 나가기
+			else if (act == 0) {
+				break;
+			}
+			// 유효성
+			else {
+				System.out.println("다시입력부탁드립니다.");
+			}
+		}
+
+		return nData;
+
+	}
 }
